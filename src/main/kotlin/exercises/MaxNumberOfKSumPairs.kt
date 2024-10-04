dc727573b6b6b7fb175ae353962c2d81be99804a
Dario Ghunney Ware <dariogware@gmail.com>
2024-10-04 15:46:19 +0100

    LC 75 questions

---
+++ src/main/kotlin/exercises/MaxNumberOfKSumPairs.kt
@@ -0,0 +1,32 @@
+package exercises
+
+class MaxNumberOfKSumPairs {
+    fun maxOperations(
+        nums: IntArray,
+        k: Int,
+    ): Int {
+        if (nums.size == 1 && nums.first() == k) return 1
+        if (nums.size == 1 && nums.first() != k) return 0
+
+        var operations = 0
+        var i = 0
+        var j = i + 1
+
+        while (i < nums.size) {
+            if (nums[i] == -1) i++
+
+            while (j < nums.size) {
+                if (nums[i] + nums[j] == k) {
+                    nums[i] = -1
+                    nums[j] = -1
+                    operations++
+                }
+                j++
+            }
+            i++
+            j = i + 1
+        }
+
+        return operations
+    }
+}
